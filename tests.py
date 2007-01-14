dfe83c699acef28460f1d2ae25c0d9c5d2118cb7
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-01-14 23:08:42 +0000

    Cleaned up test harness.  No need for some of the legacy plone setup stuff.

--- tests.py
+++ tests.py
@@ -1,60 +1,42 @@
+from datetime import datetime
+from DateTime import DateTime
 from Testing import ZopeTestCase
-from Products.PloneTestCase import PloneTestCase
-from Products.PloneTestCase import layer
-
-DEPENDENCIES = ['Archetypes', 'ATContentTypes']
-PRODUCT_DEPENDENCIES = ['MimetypesRegistry', 'PortalTransforms']
-                        
-
-# Install all (product-) dependencies, install them too
-for dependency in PRODUCT_DEPENDENCIES + DEPENDENCIES:
-    ZopeTestCase.installProduct(dependency)
-
-PRODUCTS = list()
-PRODUCTS += DEPENDENCIES
-
-PloneTestCase.setupPloneSite(products=PRODUCTS)
-
-from Products.Five import zcml
 import p4a.common
 import p4a.calendar
 import p4a.plonecalendar
-
-class AudioTestCase(PloneTestCase.PloneTestCase):
-    def _setup(self):
-        PloneTestCase.PloneTestCase._setup(self)
-        zcml.load_config('configure.zcml', p4a.common)
-        zcml.load_config('configure.zcml', p4a.calendar)
-        zcml.load_config('configure.zcml', p4a.plonecalendar)
-
-from DateTime import DateTime
-from datetime import datetime
 from p4a.calendar.tests import EventProviderTestMixin
 from p4a.calendar.interfaces import ICalendarConfig, IEventProvider
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase import layer
 
-class ATEventProviderTest(AudioTestCase, EventProviderTestMixin):
+PloneTestCase.setupPloneSite()
+
+CalendarTestCase = PloneTestCase.PloneTestCase
+
+class ATEventProviderTest(CalendarTestCase, EventProviderTestMixin):
 
     def eventSetUp(self):
         # Create folder.
         self.folder.invokeFactory('Folder', id='calendar-folder')
-        calendarfolder = self.folder['calendar-folder']
-        id = calendarfolder.invokeFactory('Event', id='event1')
-        calendarfolder['event1'].update(title='First Event',
-                                        startDate=DateTime('2006-09-28 08:30am'),
-                                        endDate=DateTime('2006-09-28 09:30am'))
-        id = calendarfolder.invokeFactory('Event', id='event2')
-        calendarfolder['event2'].update(title='Second Event',
-                                        startDate=DateTime('2006-09-29 08:30am'),
-                                        endDate=DateTime('2006-09-29 09:30am'))
-        id = calendarfolder.invokeFactory('Event', id='meeting1')
+        cal = self.folder['calendar-folder']
+        id = cal.invokeFactory('Event', id='event1')
+        cal['event1'].update(title='First Event',
+                             startDate=DateTime('2006-09-28 08:30am'),
+                             endDate=DateTime('2006-09-28 09:30am'))
+        id = cal.invokeFactory('Event', id='event2')
+        cal['event2'].update(title='Second Event',
+                             startDate=DateTime('2006-09-29 08:30am'),
+                             endDate=DateTime('2006-09-29 09:30am'))
+        id = cal.invokeFactory('Event', id='meeting1')
         # Calling this event "Meeting" is just to have one event that does
         # not have the word "Event" in the title.
-        calendarfolder['meeting1'].update(title='First Meeting',
-                                        startDate=DateTime('2006-09-30 08:30am'),
-                                        endDate=DateTime('2006-09-30 09:30am'))
+        cal['meeting1'].update(title='First Meeting',
+                               startDate=DateTime('2006-09-30 08:30am'),
+                               endDate=DateTime('2006-09-30 09:30am'))
 
         # Activate calendaring capabilities on this folder
-        config = ICalendarConfig(calendarfolder)
+        config = ICalendarConfig(cal)
         config.calendar_activated = True
         
     def afterSetUp(self):
@@ -81,7 +63,7 @@ class TopicEventProviderTest(ATEventProviderTest):
         link = self.provider.event_creation_link()
         self.failUnlessEqual(link, "")
 
-class LocationFilterTest(AudioTestCase):
+class LocationFilterTest(CalendarTestCase):
 
     def afterSetUp(self):
         # Create folder.
@@ -112,7 +94,7 @@ def test_suite():
     suite.addTest(ZopeDocFileSuite(
         'calendar.txt',
         package='p4a.plonecalendar',
-        test_class=AudioTestCase,
+        test_class=CalendarTestCase,
         )
     )
     suite.addTests(makeSuite(ATEventProviderTest))
