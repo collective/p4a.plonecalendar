2fd72f4fb3bfe073781f9722918356786c1e5ecc
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-07-31 18:37:16 +0000

    Updated various changes files and setup.py's.

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '1.0rc1'
 
 setup(name='p4a.plonecalendar',
       version=version,
-      description="",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      description="Plone4Artists calendar add-on for Plone",
+      long_description="""p4a.plonecalendar is a calendaring add-on for the
+Plone CMS.""",
+      classifiers=[
+          'Framework :: Zope3',
+          'Framework :: Plone',
+          'Programming Language :: Python',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+      keywords='Plone4Artists ',
       author='Rocky Burt',
       author_email='rocky@serverzen.com',
-      url='',
+      url='http://www.plone4artists.org/products/plone4artistscalendar',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['p4a'],
