8f3f5b24734210b10cf0e56608399a15a0ca0d67
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-28 22:32:31 +0000

    Some simple pep8 cleanups (the coverage report generator didn't like these files otherwise).

--- p4a/plonecalendar/browser/eventfilter.py
+++ p4a/plonecalendar/browser/eventfilter.py
@@ -1,4 +1,3 @@
-from urllib import urlencode
 from zope.interface import implements, Interface
 from zope.component import adapts
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
@@ -6,16 +5,17 @@ from zope.contentprovider.interfaces import IContentProvider
 
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 
+
 class LocationFilter(object):
     """A content provider for filtering events on location"""
-    
+
     implements(IContentProvider)
     adapts(Interface, IDefaultBrowserLayer, Interface)
-    
+
     template = ZopeTwoPageTemplateFile('location_filter.pt')
-    
+
     field_name = 'location' # Name of the location field for easy overriding
-    
+
     def __init__(self, context=None, request=None, view=None):
         self.context = context
         self.request = request
@@ -23,13 +23,13 @@ class LocationFilter(object):
 
     def update(self):
         catalog = self.context.portal_catalog
-        if not self.field_name in  catalog.indexes():
+        if not self.field_name in catalog.indexes():
             self.do_render = False
             return
         locations = filter(None, catalog.uniqueValuesFor(self.field_name))
         if not '' in locations:
             self.locations = ('', ) + locations
-        self.selected = self.request.form.get(self.field_name,'')
+        self.selected = self.request.form.get(self.field_name, '')
 
         hidden_fields = []
         for key, value in self.request.form.items():
@@ -39,7 +39,7 @@ class LocationFilter(object):
         self.hidden_fields = hidden_fields
         self.url = self.request.ACTUAL_URL
         self.do_render = True
-    
+
     def render(self):
         if not self.do_render:
             return ''
@@ -47,4 +47,3 @@ class LocationFilter(object):
         # letting the provider inherit from aq.implicit, and
         # wrapping it in self, but I haven't tested.
         return self.template.__of__(self.context)(provider=self)
-         
--- p4a/plonecalendar/browser/icalendar.py
+++ p4a/plonecalendar/browser/icalendar.py
@@ -4,7 +4,9 @@ from zope import interface
 from Products.CMFCore.utils import getToolByName
 from p4a.calendar.interfaces import IEventProvider
 
+
 class IiCalendarView(interface.Interface):
+
     def has_ical_support():
         """Whether or not the current object has ical support.
         """
@@ -16,7 +18,8 @@ class IiCalendarView(interface.Interface):
     def PUT(REQUEST, RESPONSE):
         """This is a PUT method for iCalendar access.
         """
-        
+
+
 class iCalendarView(object):
     """ Export the contents of this Calendar as an iCalendar file """
 
@@ -26,14 +29,14 @@ class iCalendarView(object):
         cached = getattr(self, '__cached_ical_support', None)
         if cached is not None:
             return cached
-        
+
         ct = getToolByName(self, 'portal_calendar')
         try:
             ct.exportCalendar(events=[])
             cached = True
         except TypeError, e:
             cached = False
-        
+
         self.__cached_ical_support = cached
         return cached
 
@@ -51,10 +54,10 @@ class iCalendarView(object):
 
     def PUT(self, REQUEST, RESPONSE):
         """This is a PUT method for iCalendar access.
-        
+
         The PUT method is found on the view "icalendar.ics". This
         can be slightly confusing, as it's there is no configure.zcml
-        entry for it. 
+        entry for it.
         This is also the reason why the export view "icalendar.ics" has
         to be a template and not an attribute. As a template it is
         called by calling the view class, as an attribute, it is called
@@ -62,7 +65,8 @@ class iCalendarView(object):
         That attribute doesn't have a PUT-method and WebDAV stops working.
         """
         ct = getToolByName(self.context, 'portal_calendar')
-        ct.importCalendar(REQUEST['BODYFILE'], dest=self.context, do_action=True)
+        ct.importCalendar(REQUEST['BODYFILE'], dest=self.context,
+                          do_action=True)
         RESPONSE.setStatus(204)
         return RESPONSE
 
@@ -76,7 +80,8 @@ class iCalendarView(object):
             text.decode('utf8')
         except UnicodeDecodeError:
             try:
-                # Maybe it's Latin-1? That's a break of specs, but a common one.
+                # Maybe it's Latin-1? That's a break of specs,
+                # but a common one.
                 text = text.decode('latin1')
                 # Yup, sure is. Re-encode as utf8:
                 text = text.encode('utf8', 'replace')
@@ -84,7 +89,7 @@ class iCalendarView(object):
                 # We have no idea, what this is, so lets just reencode it
                 # as UTF8 and replace everything weird with <?>.
                 text = text.encode('utf8', 'replace').encode('utf8', 'replace')
-                
+
         ical = StringIO(text)
         ct = getToolByName(self.context, 'portal_calendar')
         items = ct.importCalendar(ical, dest=self.context, do_action=True)
@@ -97,8 +102,9 @@ class iCalendarView(object):
         import os
         import Globals
 
-        # lxml.etree introduces a new class, lxml.etree.XSLT. 
-        # The class can be given an ElementTree object to construct an XSLT transformer:
+        # lxml.etree introduces a new class, lxml.etree.XSLT.
+        # The class can be given an ElementTree object to construct an
+        # XSLT transformer:
 
         from lxml import etree
 
@@ -106,8 +112,9 @@ class iCalendarView(object):
         xslt_doc = etree.parse(f)
         transform = etree.XSLT(xslt_doc)
 
-        # You can then run the transformation on an ElementTree document by simply calling it, 
-        # and this results in another ElementTree object:
+        # You can then run the transformation on an ElementTree
+        # document by simply calling it, and this results in another
+        # ElementTree object:
 
         remote_page = urllib2.urlopen(url)
         parsed_page = etree.parse(remote_page)
@@ -116,14 +123,14 @@ class iCalendarView(object):
         ct = getToolByName(self.context, 'portal_calendar')
         items = ct.importCalendar(ical, dest=self.context, do_action=True)
         return "%s items imported" % len(items)
-    
+
     def importFormHandler(self):
         if self.request.get('file') is not None:
             ct = getToolByName(self.context, 'portal_calendar')
-            items = ct.importCalendar(self.request.get('file'), dest=self.context, do_action=True)
-            self.request.portal_status_message = "%s items imported" % len(items)
+            items = ct.importCalendar(self.request.get('file'),
+                                      dest=self.context, do_action=True)
+            self.request.portal_status_message = "%s items imported" \
+                                                 % len(items)
         if self.request.get('url') is not None:
-            self.request.portal_status_message = self.import_from_url(self.request.get('url'))
-        
-        
- 
+            self.request.portal_status_message = \
+                self.import_from_url(self.request.get('url'))
