49c315327d40f9de64dec921c356f914c25959ee
rocky <rocky@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-29 20:10:21 +0000

    Stopped using deprecated zope.app.annotation import.

--- p4a/plonecalendar/calendar.py
+++ p4a/plonecalendar/calendar.py
@@ -1,12 +1,13 @@
 from zope import component
 from zope import interface
-from zope.app.annotation import interfaces as annointerfaces
+from zope.annotation import interfaces as annoifaces
 from dateable.chronos import interfaces
+
 
 class FolderCalendarConfig(object):
     """An ICalendarConfig adapter for ATCT folder content.
     """
-    
+
     interface.implements(interfaces.ICalendarConfig)
     component.adapts(interfaces.IPossibleCalendar)
 
@@ -15,14 +16,15 @@ class FolderCalendarConfig(object):
 
     def __get_calendar_activated(self):
         return interfaces.ICalendarEnhanced.providedBy(self.context) and \
-               annointerfaces.IAttributeAnnotatable.providedBy(self.context)
+               annoifaces.IAttributeAnnotatable.providedBy(self.context)
+
     def __set_calendar_activated(self, activated):
         ifaces = interface.directlyProvidedBy(self.context)
         if activated:
             if not interfaces.ICalendarEnhanced.providedBy(self.context):
                 ifaces += interfaces.ICalendarEnhanced
-            if not annointerfaces.IAttributeAnnotatable.providedBy(self.context):
-                ifaces += annointerfaces.IAttributeAnnotatable
+            if not annoifaces.IAttributeAnnotatable.providedBy(self.context):
+                ifaces += annoifaces.IAttributeAnnotatable
             if getattr(self.context, 'layout', None) is not None:
                 self.context.layout = 'month.html'
         else:
