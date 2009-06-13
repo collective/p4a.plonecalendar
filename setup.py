4e0fdd21953b67e3dfb70b75c53ae01324c15a58
lennartregebro <lennartregebro@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-06-13 09:42:40 +0000

    I don't know why I added that. It shouldn't be there.

--- setup.py
+++ setup.py
@@ -38,6 +38,5 @@ setup(name='p4a.plonecalendar',
           'dateable.kalends',
           'p4a.calendar >= 1.2dev',
           'p4a.subtyper >= 1.1, <=1.1.9999',
-          'p4a.ploneevent',
       ],
       )
